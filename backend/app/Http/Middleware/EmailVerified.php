9377e7868fa59029468a71651f7259dc2e83a3b5
CarlsonBuma <75643791+CarlsonBuma@users.noreply.github.com>
2023-08-27 19:13:35 +0200

    bugfix: middleware email_verified - throw Exception - v.4.0.3

--- backend/app/Http/Middleware/EmailVerified.php
+++ backend/app/Http/Middleware/EmailVerified.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Exception;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,7 +13,7 @@ class EmailVerified
     {   
         try {
             $user = Auth::user();
-            if(!$user) throw 'Unauthenticated! Please login again.';
+            if(!$user) throw new Exception('Not verified.');
             else if($user->email_verified_at) return $next($request);
             else $user->token()->delete();
         } catch (Exception $e) {
